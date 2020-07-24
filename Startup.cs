0833f8e9a744a61e882f0deac7b891419a706a7f
Atravel <a.velushov@gmail.com>
2020-07-24 15:20:56 +0300

    Comlete DataSeed

--- Startup.cs
+++ Startup.cs
@@ -22,20 +22,25 @@ namespace MyWebsight
         {
             //dotnet user-secrets set secretConnectionString "User ID=pdev;Password=pdev;Server=localhost;Port=5432;Database=Advantage.API.Dev;Integrated Security=true;Pooling=true;"
             //dotnet ef migrations add InitialMigration
+            //dotnet ef database update
             _connectionString = Configuration["secretConnectionString"];
             services.AddMvc();
             services.AddEntityFrameworkNpgsql()
             .AddDbContext<ApiContext>(
                 opt => opt.UseNpgsql(_connectionString));
+
+            services.AddTransient<DataSeed> ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, DataSeed seed)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            seed.SeedData (20, 1000);
 
             app.UseMvc();
         }
